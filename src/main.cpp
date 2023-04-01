4a2b6d8bf8d4f240ff74a6ffa9a9974e1617670a
N. Melih Sensoy <nmelihsensoy@gmail.com>
2023-04-01 12:56:15 +0300

    update camera resolution & fix led flicker

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,12 @@
 static const char *TAG = "example";
 static const char *REST_TAG = "esp-rest";
 
+#define FLASH_GPIO_NUM 4
+
 // ===================
 // Sdcard
 // ===================
 #define MOUNT_POINT "/sdcard"
-#define PIN_NUM_MISO 2
-#define PIN_NUM_MOSI 15
-#define PIN_NUM_CLK  14
-#define PIN_NUM_CS   13
 
 // ===================
 // Access Point
@@ -444,30 +442,13 @@ void init_camera(){
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_HD;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
-  
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    if(psramFound()){
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-  }
 
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -475,23 +456,16 @@ void init_camera(){
     return;
   }
 
-  sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
+  //sensor_t * s = esp_camera_sensor_get();
+  //s->set_framesize(s, FRAMESIZE_QVGA);
 }
 
 void setup(){
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
+  pinMode(FLASH_GPIO_NUM, OUTPUT);
+  digitalWrite(FLASH_GPIO_NUM, HIGH);
 
   // camera init
   init_camera();
